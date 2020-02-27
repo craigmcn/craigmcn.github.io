99186dd9e87d334d7cf008fdfb1ef6ce4fdf84cd
Craig McNaughton <therealcraigmcn@gmail.com>
2020-02-27 14:42:05 -0800

    Create 2020-03-02-building-a-mystery.m

---
+++ _posts/2020-03-02-building-a-mystery.m
@@ -0,0 +1,27 @@
+---
+title: "Building a Mystery"
+date: 2020-03-02T11:45:00-0800
+excerpt: "This week’s instalment of Music Monday is Building a Mystery. The 1997 Sarah McLachlan original and a 2020 cover by Justin Rutledge."
+layout: post
+categories:
+- People
+- Music Monday
+tags:
+- covers
+- music
+- video
+- Sarah McLachlan
+- Justin Rutledge
+---
+This week’s instalment of Music Monday is [_Building a Mystery_](https://en.wikipedia.org/wiki/Building_a_Mystery) by
+[Sarah McLachlan](http://sarahmclachlan.com/) from her 1997 album,
+[Surfacing](https://en.wikipedia.org/wiki/Surfacing_(album)). The [Justin Rutledge](https://justinrutledge.squarespace.com/)
+version is from a 2020 performance for [CBC Music’s Junos 365 Sessions](https://www.cbc.ca/music/junos/watch/it-s-a-real-mouthful-of-a-song-justin-rutledge-covers-sarah-mclachlan-1.5475586).
+
+<div class="video-container">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/zWxzM9_gEo0" frameborder="0" allowfullscreen title="Video: Building a Mystery by Sarah McLachlan"></iframe>
+</div>
+
+<div class="video-container">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/Mwzzrdk7hyE" frameborder="0" allowfullscreen title="Video: Building a Mystery by Justin Rutledge"></iframe>
+</div>
